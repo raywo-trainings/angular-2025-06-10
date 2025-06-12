3e8b1b9939f0f5191695ba883cccfb42f90a5a72
Ray Wojciechowski <ray.wojciechowski@ars.de>
2025-06-12 09:43:00 +0200

    Add dummy data and basic CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RecipeList} from './recipes/components/recipe-list/recipe-list';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RecipeList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'recipes';
+
 }
